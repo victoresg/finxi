a05c7a7509d3dbe5f9b80d56ab78f4167d727c70
Victor Elias <elias.victor@ipepar.com>
2020-10-31 20:05:31 -0300

    Updated image in footer.

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="footer d-none d-md-block">
-    <div class="row no-gutters">
-      <div>
-        footer
-      </div>
+  <footer>
+    <div class="container">
+      <img src='../../assets/darth-vader-footer.png' width="70">
+      <img src='../../assets/star-wars-logo.png' width="150">
+      <img src='../../assets/luke.png' width="40">
     </div>
   </footer>
 </template>
@@ -15,10 +15,24 @@ export default {
 </script>
 
 <style lang="css" scoped>
-  .footer {
+  footer {
     position: fixed;
     bottom: 0;
     background-color: #1a93b2;
     width: 100%;
+  }
+  footer .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 15px;
+  }
+
+  /* responsive */
+
+  @media(max-width: 768px) {
+    footer .container img:nth-of-type(2) {
+      width: 100px;
+    }
   }
 </style>
